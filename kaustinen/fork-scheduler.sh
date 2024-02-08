47415fcb6ed8d8a322343d8cc6b0a72eae7a8b8d
gajinder <develop@g11tech.io>
2024-02-08 15:55:10 +0530

    some updates

--- kaustinen/fork-scheduler.sh
+++ kaustinen/fork-scheduler.sh
@@ -21,8 +21,10 @@ echo "configDir=$configDir"
 SECONDS_PER_SLOT=6
 SLOTS_PER_EPOCH=8
 
-GENESIS_TIME="$(date +%s)"
-GENESIS_TIME=$((GENESIS_TIME + 30))
+ELGENESIS_TIME="$(date +%s)"
+GENESIS_TIME=$((ELGENESIS_TIME + 30))
+# set shanghai at el genesis
+SHANGHAI_TIME="$ELGENESIS_TIME"
 PRAGUE_TIME=$((GENESIS_TIME + SECONDS_PER_SLOT*SLOTS_PER_EPOCH*VERGE_EPOCH))
 
 # if there is no transition set PRAGUE_TIME 1 hour back so that we don't
@@ -33,7 +35,15 @@ then
   echo "since VERGE_EPOCH=$VERGE_EPOCH setting pragueTime to 1 hour back to avoid pre image caching issue"
 fi;
 
-echo "GENESIS_TIME=$GENESIS_TIME PRAGUE_TIME=$PRAGUE_TIME"
+echo "ELGENESIS_TIME=$ELGENESIS_TIME GENESIS_TIME=$GENESIS_TIME SHANGHAI_TIME=$SHANGHAI_TIME PRAGUE_TIME=$PRAGUE_TIME"
+
+sedPatten="/timestamp/c\    \"timestamp\":$ELGENESIS_TIME,"
+echo $sedPatten
+sed -i "$sedPatten" "$configDir/genesis.json"
+
+sedPatten="/shanghaiTime/c\    \"shanghaiTime\":$SHANGHAI_TIME,"
+echo $sedPatten
+sed -i "$sedPatten" "$configDir/genesis.json"
 
 sedPatten="/pragueTime/c\    \"pragueTime\":$PRAGUE_TIME,"
 echo $sedPatten
